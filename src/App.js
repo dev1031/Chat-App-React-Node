91c782bfc3b4c748887c2dff1b55cb9c6ee6950f
dherendra dev <dheerudev101@gmail.com>
2020-08-18 13:59:10 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router,Switch,Route } from "react-router-dom";
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
+
+function App() {
+  return (
+    <Router>
+    <Switch>
+      <Route exact path={'/'} component ={Join} />
+      <Route exact path={'/chat'} component ={Chat} />
+    </Switch>
+    </Router>
+  );
+}
+
+export default App;
